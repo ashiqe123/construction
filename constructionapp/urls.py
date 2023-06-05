4c500509af432ffbf795e5b5fdae45b24c4140b7
ashiqe123 <muhammedashiqva@gmail.com>
2023-06-05 20:18:25 +0530

    secrr

--- constructionapp/urls.py
+++ constructionapp/urls.py
@@ -4,4 +4,10 @@ urlpatterns=[
     path('',views.home,name='home'),
     path("log",views.log,name='log'),
     path('sig',views.sig,name='sig'),
+    path('services',views.services,name='services'),
+    path('meterial',views.meterial,name='meterial'),
+    path('dt',views.dt,name='dt'),
+    path('home',views.home,name='home'),
+    path('lgout',views.lgout,name='lgout'),
+    path('about',views.about,name='about')
 ]
--- constructionapp/views.py
+++ constructionapp/views.py
@@ -9,3 +9,21 @@ def log(request):
 
 def sig(request):
     return render(request,"signin.html")
+
+def services(request):
+    return render(request,"services.html")
+
+def meterial(request):
+    return render(request,"meterials.html")
+
+def dt(request):
+    return render(request,'dt.html')
+
+def home(request):
+    return render(request,'home.html')
+
+def lgout(request):
+    return render(request,"index.html")
+
+def about(request):
+    return render(request,"about.html")
